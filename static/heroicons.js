3c373302ed49b05a2a3271f1e7761cb421079f4a
NateAFish <47712194+izukuuuu@users.noreply.github.com>
2025-10-06 11:44:22 +0800

    Refine home workspace actions layout

---
+++ static/heroicons.js
@@ -0,0 +1,134 @@
+(function(){
+  // Heroicons outline icons bundled locally to avoid external network requests.
+  // Icons are sourced from https://github.com/tailwindlabs/heroicons (MIT License).
+  const NS = 'http://www.w3.org/2000/svg';
+  const defaultViewBox = '0 0 24 24';
+  const defaultStrokeWidth = 1.5;
+  const defaultPathAttrs = { 'stroke-linecap': 'round', 'stroke-linejoin': 'round' };
+
+  const icons = {
+    'clipboard-document-list': {
+      paths: [
+        "M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+      ]
+    },
+    'home-modern': {
+      paths: [
+        "M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
+      ]
+    },
+    'chevron-left': {
+      paths: [
+        "M15.75 19.5 8.25 12l7.5-7.5"
+      ]
+    },
+    'chevron-right': {
+      paths: [
+        "m8.25 4.5 7.5 7.5-7.5 7.5"
+      ]
+    },
+    'adjustments-horizontal': {
+      paths: [
+        "M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+      ]
+    },
+    'chart-pie': {
+      paths: [
+        "M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z",
+        "M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+      ]
+    },
+    'cog-6-tooth': {
+      paths: [
+        "M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z",
+        "M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+      ]
+    },
+    'lock-closed': {
+      paths: [
+        "M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+      ]
+    },
+    'lock-open': {
+      paths: [
+        "M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+      ]
+    },
+    'arrow-up-tray': {
+      paths: [
+        "M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+      ]
+    },
+    'cloud-arrow-up': {
+      paths: [
+        "M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+      ]
+    },
+    'folder-arrow-down': {
+      paths: [
+        "m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+      ]
+    },
+    'document-arrow-down': {
+      paths: [
+        "M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+      ]
+    },
+    'arrow-down-tray': {
+      paths: [
+        "M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+      ]
+    }
+  };
+
+  function applyIcon(el, name){
+    if(!el) return;
+    const icon = icons[name];
+    if(!icon) return;
+    while(el.firstChild){ el.removeChild(el.firstChild); }
+    const viewBox = icon.viewBox || defaultViewBox;
+    el.setAttribute('viewBox', viewBox);
+    el.setAttribute('fill', 'none');
+    el.setAttribute('stroke', 'currentColor');
+    el.setAttribute('stroke-width', icon.strokeWidth || defaultStrokeWidth);
+    if(!el.hasAttribute('aria-hidden')){
+      el.setAttribute('aria-hidden', 'true');
+    }
+    const paths = Array.isArray(icon.paths) ? icon.paths : [];
+    paths.forEach((pathData)=>{
+      if(!pathData) return;
+      const path = document.createElementNS(NS, 'path');
+      if(typeof pathData === 'string'){
+        path.setAttribute('d', pathData);
+        Object.entries(defaultPathAttrs).forEach(([k,v])=>path.setAttribute(k,v));
+      }else if(typeof pathData === 'object'){
+        if(pathData.d){ path.setAttribute('d', pathData.d); }
+        Object.entries({ ...defaultPathAttrs, ...(pathData.attrs||{}) }).forEach(([k,v])=>{
+          if(v!==undefined){ path.setAttribute(k,v); }
+        });
+      }
+      el.appendChild(path);
+    });
+    el.dataset.heroicon = name;
+  }
+
+  function refresh(root){
+    const scope = root || document;
+    scope.querySelectorAll('svg[data-heroicon]').forEach((el)=>{
+      const name = el.dataset.heroicon;
+      if(name){ applyIcon(el, name); }
+    });
+  }
+
+  window.Heroicons = {
+    apply: applyIcon,
+    refresh,
+    icons
+  };
+
+  if(document.readyState === 'loading'){
+    document.addEventListener('DOMContentLoaded', ()=>refresh(document));
+  }else{
+    refresh(document);
+  }
+})();
